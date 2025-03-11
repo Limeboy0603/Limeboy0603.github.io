2ddb6d0ce885a4d3fa6b25867a201fc72b3b8bc7
Limeboy <56017871+Limeboy0603@users.noreply.github.com>
2025-03-11 21:01:48 +0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -11,23 +11,26 @@ var helptext=
 "github: opens my GitHub profile on a new tab.<br>";
 
 var infotext=
-"My username is Limeboy0603. I am currently a year 2 university student in City University of Hong Kong, major in computer science. <br>"+
+"My username is Limeboy0603. I am currently a year 4 university student in City University of Hong Kong, major in computer science. <br>"+
 "My interest is programing.<br><br>"+
 "My projects:<br><ul>"+
 "<li>KTaNE modded module: osu! <a href=\"https://github.com/Limeboy0603/ktaneOsu--master\" target=\"_blank\">GitHub</a></li>"+
 "<li>KTaNE modded module: Minecraft Cipher <a href=\"https://github.com/Limeboy0603/ktaneMinecraftCipher-master\" target=\"_blank\">GitHub</a></li>"+
 "<li>KTaNE modded module: Minecraft Parody <a href=\"https://github.com/Limeboy0603/ktaneMinecraftParody-master\" target=\"_blank\">GitHub</a></li></ul>";
 
+var githubtext = "GitHub profile has been opened in a new tab.";
+
+var webtext = "Website version: 1.0.2    <a href=\"https://github.com/Limeboy0603/Limeboy0603.github.io\">Website Repository</a>"
+
 const text = {
     help: helptext,
     info: infotext,
-    github: "GitHub profile has been opened in a new tab.",
-    web: "Website version: 1.0.2    <a href=\"https://github.com/Limeboy0603/Limeboy0603.github.io\">Website Repository</a>",
+    github: githubtext,
+    web: webtext,
     home: hometext
 };
 
-function setrespond()
-{
+function setrespond() {
     var command = document.getElementsByTagName("input")[0].value.toLowerCase();
     if(text[command]===undefined)
         document.getElementById("console-output").innerHTML=errortext;
@@ -39,16 +42,13 @@ function setrespond()
     document.getElementsByTagName("input")[0].value="";
 }
 
-document.addEventListener("keypress",function(event)
-{
-    if(event.key === "Enter")
-    {
+document.addEventListener("keypress",function(event) {
+    if(event.key === "Enter") {
         event.preventDefault();
         setrespond();
     }
 });
 
-function main()
-{
+function main() {
     document.getElementById("console-output").innerHTML=text["home"];
 }
